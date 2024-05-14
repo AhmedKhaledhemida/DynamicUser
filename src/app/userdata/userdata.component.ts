b3985fd51fbe3841439f5621eb07232174fd5845
AhmedKhaledhemida <ahmedkhaledhemida1@gmail.com>
2024-05-14 14:50:23 +0300

    update

--- src/app/userdata/userdata.component.ts
+++ src/app/userdata/userdata.component.ts
@@ -1,49 +1,4 @@
-// import { Component, OnInit } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
 
-// @Component({
-//   selector: 'app-userdata',
-//   templateUrl: './userdata.component.html',
-//   styleUrls: ['./userdata.component.css']
-// })
-// export class UserdataComponent implements OnInit {
-//   users: any[] = [];
-//   currentPage: number = 1;
-//   totalPages: number = 0;
-//   usersLoaded = false;
-//   constructor(private http: HttpClient) {}
-
-//   ngOnInit(): void {
-//     this.getData();
-//   }
-
-//   async getData() {
-//     try {
-//       const response = await this.http
-//         .get<any>(`https://reqres.in/api/users?page=${this.currentPage}`)
-//         .toPromise();
-//       this.users = response.data;
-//       this.totalPages = response.total_pages;
-//       this.usersLoaded = true; // Set usersLoaded to true after data is fetched
-//     } catch (error) {
-//       console.error('Error fetching data:', error);
-//     }
-//   }
-
-//   nextPage() {
-//     if (this.currentPage < this.totalPages) {
-//       this.currentPage++;
-//       this.getData();
-//     }
-//   }
-
-//   prevPage() {
-//     if (this.currentPage > 1) {
-//       this.currentPage--;
-//       this.getData();
-//     }
-//   }
-// }
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -57,6 +12,8 @@ export class UserdataComponent implements OnInit {
   currentPage: number = 1;
   totalPages: number = 0;
   usersLoaded = false;
+  cache: { [page: number]: any[] } = {};
+
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
@@ -70,15 +27,23 @@ export class UserdataComponent implements OnInit {
   }
 
   async getData() {
-    try {
-      const response = await this.http
-        .get<any>(`https://reqres.in/api/users?page=${this.currentPage}`)
-        .toPromise();
-      this.users = response.data;
-      this.totalPages = response.total_pages;
+    if (this.cache[this.currentPage]) {
+
+      this.users = this.cache[this.currentPage];
       this.usersLoaded = true;
-    } catch (error) {
-      console.error('Error fetching data:', error);
+    } else {
+      try {
+        const response = await this.http
+          .get<any>(`https://reqres.in/api/users?page=${this.currentPage}`)
+          .toPromise();
+        this.users = response.data;
+        this.totalPages = response.total_pages;
+        this.usersLoaded = true;
+
+        this.cache[this.currentPage] = response.data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     }
   }
 
--- src/app/userinfo/userinfo.component.ts
+++ src/app/userinfo/userinfo.component.ts
@@ -1,38 +1,4 @@
-// import { Component, OnInit } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { ActivatedRoute } from '@angular/router';
 
-// @Component({
-//   selector: 'app-userinfo',
-//   templateUrl: './userinfo.component.html',
-//   styleUrls: ['./userinfo.component.css']
-// })
-// export class UserinfoComponent implements OnInit{
-//   users: any = null;
-//   id: any;
-//   usersloaded=false;
-//   constructor(private http: HttpClient, private route: ActivatedRoute) {}
-
-//   ngOnInit(): void {
-//     this.route.params.subscribe((params) => {
-//       this.id = params['id'];
-//       this.getData(this.id);
-//     });
-//   }
-
-//   async getData(id: number) { // Accept id parameter here
-//     try {
-//       const data = await this.http
-//         .get<any>(`https://reqres.in/api/users/${id}`)
-//         .toPromise();
-//       this.users = data;
-//     console.log(this.users);
-
-//     } catch (error) {
-//       console.error('Error fetching user data:', error);
-//     }
-//   }
-// }
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
@@ -63,7 +29,7 @@ export class UserinfoComponent implements OnInit {
         .toPromise();
       this.users = data;
       setTimeout(() => {
-        this.usersLoaded = true; // Set usersLoaded to true after 1000 milliseconds
+        this.usersLoaded = true;
       }, 500);
     } catch (error) {
       console.error('Error fetching user data:', error);
